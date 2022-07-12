adb28c095c520322ddf13307916ee85240b9f5a2
moogah <moogah@gmail.com>
2022-07-11 22:35:52 -0400

    added python dev setup with elpy and autopep8

--- emacs/init.el
+++ emacs/init.el
@@ -1,3 +1,7 @@
+;; ===============================================================================
+;; configure MELPA
+;; ===============================================================================
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
@@ -5,27 +9,66 @@
 ;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
+;; ===============================================================================
 ;; Install Packages https://wikemacs.org/wiki/Package.el
+;; ===============================================================================
+
 (require 'cl-lib)
 
 (defvar my-packages
-  '(magit) "A list of packages to ensure are installed at launch")
+  '(better-defaults
+    elpy
+    flycheck
+    py-autopep8
+    blacken
+    magit
+    monokai-theme
+    vs-light-theme
+    material-theme
+    org-roam
+    )
+  "A list of packages to ensure are installed at launch")
 
-(defun my-packages-installed-p ()
-  (cl-loop for p in my-packages
-	   when (not (package-installed-p p)) do (cl-return nil)
-	   finally (cl-return t)))
+(mapc #'(lambda (package)
+	  (unless (package-installed-p package)
+	    (package-install package)))
+      my-packages)
 
-(unless (my-packages-installed-p)
-  ;; check for new packages (package versions)
-  (package-refresh-contents)
-  ;; install the missing packages
-  (dolist (p my-packages)
-    (when (not (package-installed-p p))
-      (package-install p))))
+;; ===============================================================================
+;; Basic Configuration
+;; ===============================================================================
+
+(global-linum-mode t)
+;; Show line number in the mode line.
+(line-number-mode 1)
+;; Show column number in the mode line.
+(column-number-mode 1)
+
+(load-theme 'vs-light t)
 
 
+;; ===============================================================================
+;; python development config
+;; ===============================================================================
+
+;; enable elpy
+(elpy-enable)
+
+;; disable indentation highlights
+(setq elpy-modules (delq 'elpy-module-highlight-indentation elpy-modules))
+
+;; Enable flycheck
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+;; Enable autopep8
+(require 'py-autopep8)
+(add-hook 'python-mode-hook 'py-autopep8-mode)
+
+;; ===============================================================================
 ;; Configs from pragmaticemacs.wordpress.com Org-Mode TODO
+;; ===============================================================================
 
 ;; set key for agenda
 (global-set-key (kbd "C-c a") 'org-agenda)
@@ -57,7 +100,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(magit)))
+ '(custom-safe-themes
+   '("90a6f96a4665a6a56e36dec873a15cbedf761c51ec08dd993d6604e32dd45940" "f149d9986497e8877e0bd1981d1bef8c8a6d35be7d82cba193ad7e46f0989f6a" "a9318f38c2d39f717d61aa0c155f579fc3a369c4a0d01f4848de0dee85fbd831" "78e6be576f4a526d212d5f9a8798e5706990216e9be10174e3f3b015b8662e27" "fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" "4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" default))
+ '(package-selected-packages
+   '(blacken py-autopep8 flycheck elpy better-defaults material-theme vs-light-theme monokai-theme solarized-theme magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
