a2f15633ef6ce80e3cad900ff6f7ecb3f4b11331
Jeff Farr <jefarr@wayfair.com>
2023-01-20 08:51:23 -0500

    checkpoint

--- emacs/major-modes/magit.el
+++ emacs/major-modes/magit.el
@@ -14,10 +14,16 @@
   :config
   (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1))
 
-(use-package forge
-  :straight t
-  :after magit
-  :config
-  (push '("github.csnzoo.com" "github.csnzoo.com/api/v3"
-          "github.csnzoo.com" forge-github-repository)
-        forge-alist))
+(use-package browse-at-remote
+   :straight t
+   :config
+   (add-to-list 'browse-at-remote-remote-type-regexps '("^github\\.csnzoo\\.com$" . "github")))
+
+
+;; (use-package forge
+;;   :straight t
+;;   :after magit
+;;   :config
+;;   (push '("github.csnzoo.com" "github.csnzoo.com/api/v3"
+;;           "github.csnzoo.com" forge-github-repository)
+;;         forge-alist))
