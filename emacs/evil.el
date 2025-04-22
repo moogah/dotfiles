41f0cf2b43a44dd98132f3ca43c72e53c9393327
moogah <moogah@gmail.com>
2025-04-22 10:58:44 -0500

    removed unused evil packages

--- emacs/evil.el
+++ emacs/evil.el
@@ -5,39 +5,31 @@
 (use-package evil
   :straight t
   :config
-  (use-package evil-surround
-    :straight t
-    :config
-    (global-evil-surround-mode 1))
-  (use-package evil-matchit
-    :straight t
-    :config
-    (global-evil-matchit-mode 1))
   (evil-mode 1)
   (setq evil-want-fine-undo t)
   (add-hook 'org-capture-mode-hook 'evil-insert-state) ;; use insert by default for org capture
   (add-hook 'git-commit-mode-hook 'evil-insert-state) ;; use insert mode by default for magit commits
   (when (dirvish-override-dired-mode))
-    (evil-set-initial-state 'dired-mode 'emacs)
-  (use-package goto-chg
-    :straight t))
+    (evil-set-initial-state 'dired-mode 'emacs))
 
 ;; add visual indicators for common vim commands
 (use-package evil-goggles
   :straight t
+  :after evil
   :config
   (evil-goggles-mode))
 
-(use-package evil-args
+(use-package evil-surround
   :straight t
+  :after evil
   :config
-  (define-key evil-inner-text-objects-map "a" 'evil-inner-arg)
-  (define-key evil-outer-text-objects-map "a" 'evil-inner-arg)
-  (define-key evil-normal-state-map "L" 'evil-forward-arg)
-  (define-key evil-normal-state-map "H" 'evil-backward-arg)
-  (define-key evil-motion-state-map "L" 'evil-forward-arg)
-  (define-key evil-motion-state-map "H" 'evil-backward-arg)
-  (define-key evil-normal-state-map "K" 'evil-jump-out-args))
+  (global-evil-surround-mode 1))
+
+(use-package evil-matchit
+  :straight t
+  :after evil
+  :config
+  (global-evil-matchit-mode 1))
 
 ;; ===============================================================================
 ;; Evil Collection provides vim-like bindings for many emacs modes
