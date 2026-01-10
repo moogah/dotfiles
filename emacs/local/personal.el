f3c52662fe856e650fdf6a980980b4bbcf281714
Jeff Farr <moogah@gmail.com>
2026-01-10 13:44:38 +0100

    personal local config

---
+++ emacs/local/personal.el
@@ -0,0 +1,10 @@
+;; -*- lexical-binding: t; -*-
+;; Machine-specific configuration for Mac.home
+
+;; Configure hostname-based daily journal directory
+(setq org-roam-dailies-directory "dailies/")
+
+(setq browser-hist-db-paths
+      '((safari . "/Users/jefffarr/Library/Safari/History.db")))
+
+(setq browser-hist-default-browser 'safari)
